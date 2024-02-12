eec28326aca685a9580d43b92a1c6457b3fd5c51
omuomugin <m@omuomugin.com>
2024-02-13 03:12:43 +0900

    email updated

--- scripts/git_setup.sh
+++ scripts/git_setup.sh
@@ -1,5 +1,5 @@
 echo "[git setup] ssh key generating"	
-ssh-keygen -t rsa -b 4096 -C "omuomugin@out-box.dev"	
+ssh-keygen -t rsa -b 4096 -C "m@omuomugin.com"	
 
 echo "[git setup] ssh key copied to clipboard"	
 pbcopy < ~/.ssh/id_rsa.pub
